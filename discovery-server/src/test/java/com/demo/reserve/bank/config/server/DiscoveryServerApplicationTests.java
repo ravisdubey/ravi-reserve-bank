e91193240589a5ea444d8d60ee25f98eed17e19b
ravisdubey <dubeyravi85423@gmail.com>
2025-07-06 19:48:27 +0530

    discovery-server

---
+++ discovery-server/src/test/java/com/demo/reserve/bank/config/server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.reserve.bank.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
